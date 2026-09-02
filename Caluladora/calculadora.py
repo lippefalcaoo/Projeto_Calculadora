394940b25f8a32f1c215620e9fe3ac8a03ee916d
lippefalcaoo <lippefalcaoo@gmail.com>
2026-09-01 21:51:47 -0300

    reformulei a calculadora inteira

--- Caluladora/calculadora.py
+++ Caluladora/calculadora.py
@@ -9,43 +9,39 @@ while True:
     opcao = int(input('\ndigite o número da operação desejada: '))
 
     if opcao == 0:
-        print(f'\nfim da calculadora')
+        print(f'\n----fim da calculadora----')
         break
 
-    elif opcao == 1:   
-        print(f'\n\t----SOMA----')
+    elif opcao in [1, 2, 3, 4]:   
         num1 = float(input('\ndigite o primeiro número: '))
         num2 = float(input('digite o segundo número: '))
-        resultado = num1 + num2
-        print(f'\n{num1} + {num2} = {resultado}')
-        print(f'\nRESULTADO: {resultado:.2f}')
-
-    elif opcao == 2:
-        print(f'\n\t----SUBTRAÇÃO----')
-        num1 = float(input('\ndigite o primeiro número: '))
-        num2 = float(input('digite o segundo número: '))
-        resultado = num1 - num2
-        print(f'\n{num1} - {num2} = {resultado}')
-        print(f'\nRESULTADO: {resultado:.2f}')
-
-    elif opcao == 3:
-        print(f'\n\t----MULTIPLICAÇÃO----')
-        num1 = float(input('\ndigite o primeiro número: '))
-        num2 = float(input('digite o segundo número: '))
-        resultado = num1 * num2
-        print(f'\n{num1} x {num2} = {resultado}')
-        print(f'\nRESULTADO: {resultado:.2f}')
-
-    elif opcao == 4:
-        print(f'\n\t----DIVISÃO----')
-        num1 = float(input('\ndigite o primeiro número: '))
-        num2 = float(input('digite o segundo número: '))
-        if num2 == 0:
-            print(f'\nNão é possível dividir por zero!')
-        else:
-            resultado = num1 / num2
-            print(f'\n{num1} ÷ {num2} = {resultado}')
+        
+        if opcao == 1:
+            print(f'\n\t----SOMA----')
+            resultado = num1 + num2
+            print(f'\n{num1} + {num2} = {resultado}')
             print(f'\nRESULTADO: {resultado:.2f}')
+
+        elif opcao == 2:
+            print(f'\n\t----SUBTRAÇÃO----')
+            resultado = num1 - num2
+            print(f'\n{num1} - {num2} = {resultado}')
+            print(f'\nRESULTADO: {resultado:.2f}')
+
+        elif opcao == 3:
+            print(f'\n\t----MULTIPLICAÇÃO----')
+            resultado = num1 * num2
+            print(f'\n{num1} x {num2} = {resultado:.5f}')
+            print(f'\nRESULTADO: {resultado:.2f}')
+
+        elif opcao == 4:
+            print(f'\n\t----DIVISÃO----')
+            if num2 == 0:
+                print(f'\nNão é possível dividir por zero!')
+            else:
+                resultado = num1 / num2
+                print(f'\n{num1} ÷ {num2} = {resultado:.5f}')
+                print(f'\nRESULTADO: {resultado:.2f}')
 
     else:
         print(f'\nopção inválida, tente novamente!')
